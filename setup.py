1df8d9b22d7c737225a380a0d1fd62a65c2b9ead
M Muchsin <87637609+mmuchsin@users.noreply.github.com>
2022-11-19 09:48:01 +0700

    update version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ipynb-renderer-ucn",
-    version="0.0.1",
+    version="0.2.1",
     author="muchsin",
     description="IPython Notebooks Renderer",
     long_description=open("README.md").read(),
